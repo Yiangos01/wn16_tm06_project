ff0312de2a303d01062af5222f8b363df69a4b9b
diann28 <dioann28@gmail.com>
2016-11-08 20:49:04 +0200

    add javaScript

--- EPL425-project/index.php
+++ EPL425-project/index.php
@@ -6,8 +6,59 @@
 
 
   <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+  <?php
+    $str = file_get_contents('https://api.foody.com.cy/branch/multimenu/13?auth=fe5df6b03af002742b6539e6122009e35538b24c&skey=SKEY_5821c4ca697f6&XDEBUG_SESSION_START=PHPSTORM');
+    $json = json_decode($str,true);
+   ?>
+   <link rel="stylesheet" href="css/style.css">
 
-      <link rel="stylesheet" href="css/style.css">
+   <script type="text/javascript">
+   var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+    };
+    getJSON("https://api.foody.com.cy/branch/multimenu/13?auth=fe5df6b03af002742b6539e6122009e35538b24c&skey=SKEY_5821c4ca697f6&XDEBUG_SESSION_START=PHPSTORM",
+      function(err, data) {
+        if (err != null) {
+          alert("Something went wrong: " + err);
+        } else {
+          //var cat = document.getElementyId("categories")
+          //test.innerHTML=data.categories[0].name;
+          for(var obj in data.categories){
+            categories.innerHTML +='<li id="obj" onclick="fai(\''+ menu(data.categories[obj]) +'\')"><a href= \"#\">' + data.categories[obj].name +'</a></li>';
+          }
+
+          alert("Your query count: " + data.query.count);
+        }
+      });
+
+      function menu(categories){
+        var button
+        for(var obj in categories.menuitems){
+          button += "<button id=\"btn\" type=\"button\" class=\"btn btn-default btn-lg\">"+ categories.menuitems[obj].name + "</button>";
+        }
+        //return button;
+      }
+
+
+    function fai(cunt){
+      //menuitems.innerHTML= '<button id="btn" type="button" class=\"btn btn-default btn-lg\">'+ cunt + '</button>';
+      //var categories = document.getElementyId(obj);
+      //for(var obj in cat.menuitems){
+        //menuitems.innerHTML += '<button id="btn" type="button" class=\"btn btn-default btn-lg\">'+ cat.menuitems[obj].name + '</button>';
+      //}
+    }
+   </script>
 
 </head>
 
@@ -15,6 +66,7 @@
 
 
 <body>
+  <div id="test"></div>
   <nav class="navbar navbar-inverse navbar-static-top">
   <div class="container">
     <a href="#" class="navbar-brand ">POS</a>
@@ -44,25 +96,18 @@
 
 
   <div  class="col-xs-4 col-md-4">
-    <ul class="nav nav-pills nav-stacked">
-      <li id="l" ><a href="#">1</a></li>
-      <li id="l"><a href="#">2</a></li>
-      <li id="l" class="active"><a href="#">3</a></li>
-      <li id="l"><a href="#">4</a></li>
-      <li id="l"><a href="#">5</a></li>
+    <ul class="nav nav-pills nav-stacked" id="categories">
+       <!--
+      echo '<pre>' . print_r($json,true) . '</pre>';
+      foreach ($json['categories'] as $field => $value) {
+          $temp=$value['name'];
+            echo "<li id=$field ><a href=\"#\">$temp</a></li>";
+    }     -->
     </ul>
   </div>
 
   <div class="col-xs-8 col-sm-6 col-md-8">
-    <p id="h">
-        <?php
-          for($i=0;$i<5;$i++){
-         ?>
-          <button id="btn" type="button" class="btn btn-default btn-lg">Large button</button>
-          <?php
-        }
-        ?>
-    </p>
+    <p id="menuitems"></p>
   </div>
 </div>
 </div>
--- EPL425-project/testing.php
+++ EPL425-project/testing.php
@@ -1,5 +1,12 @@
 <?php
-  $str = file_get_contents('https://api.foody.com.cy//orders/resume/57?auth=fe5df6b03af002742b6539e6122009e35538b24c&skey=SKEY_5821cb5262f35&XDEBUG_SESSION_START=PHPSTORM');
+  $str = file_get_contents('https://api.foody.com.cy/branch/multimenu/13?auth=fe5df6b03af002742b6539e6122009e35538b24c&skey=SKEY_5821c4ca697f6&XDEBUG_SESSION_START=PHPSTORM');
   $json = json_decode($str,true);
-  echo '<pre>' . print_r($json,true) . '</pre>';
+  //echo '<pre>' . print_r($json,true) . '</pre>';
+  foreach ($json['categories'] as $field => $value) {
+    $categories=$value;
+    echo '<pre>' . print_r($categories['name'],true) . '</pre>';
+    foreach ($categories['menuitems'] as $key => $Catvalue) {
+      echo '<pre>' . print_r($Catvalue['name'],true) . '</pre>';
+    }
+}
  ?>
